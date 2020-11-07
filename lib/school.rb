06d3e2fc5066444198ec244c8cc5d9f8754c0bef
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-11-07 17:25:04 -0500

    commit tree b612527ce309747c86faeb9c5deee09aec5fe5d0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require "pry"
+class School
+
+    attr_accessor  :roster, :name
+
+    def initialize(name)
+       @name = name
+        @roster = {}
+    end
+    def add_student(name, grade)
+        
+        if !roster[grade]
+            roster[grade]= []
+        end
+        
+        roster[grade] << name
+        
+    end
+   
+    def grade(grade)
+        roster[grade]
+    end
+    def sort
+        
+        @roster.each do |key, string|
+            string.sort!
+            
+        end
+    end
+end
